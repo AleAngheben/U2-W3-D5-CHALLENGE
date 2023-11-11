93c9ed5e9bdca8a0e10903ae4a526cc60a217a0a
AleAngheben <144798020+AleAngheben@users.noreply.github.com>
2023-11-12 00:36:02 +0100

    some fix

--- backoffice2.js
+++ backoffice2.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const imageUrl = document.getElementById("imageUrl");
   const price = document.getElementById("price");
 
+  const btnDelete = document.getElementById("reset-btn");
+
   const params = new URLSearchParams(window.location.search);
   const productId = params.get("productId");
 
@@ -27,7 +29,35 @@ document.addEventListener("DOMContentLoaded", async () => {
     brand.value = productDetails.brand;
     imageUrl.value = productDetails.imageUrl;
     price.value = productDetails.price;
+
+    btnDelete.innerText = "Cancella il prodotto";
   } catch (error) {
     "errore nel recupero dei valori ", error;
   }
 });
+
+// const deleteProduct = async (deleteProductId) => {
+//   try {
+//     const acceptDelete = confirm(
+//       "Sei sicuro di voler eliminare questo prodotto?"
+//     );
+
+//     if (acceptDelete) {
+//       const response = await fetch(URL + deleteProductId, {
+//         method: "DELETE",
+//         headers: {
+//           Authorization:
+//             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkMDQyNmQxYmJlMTAwMTgzOWU2MjciLCJpYXQiOjE2OTk1NDYxNTEsImV4cCI6MTcwMDc1NTc1MX0.n6bF57FpGYml_yaLtIhrUAg10VpZY-4vrUm-nkLmsbg",
+//           "Content-Type": "application/json",
+//         },
+//       });
+//       const product = await response.json();
+//       alert("hai eliminato " + product.name);
+//     }
+//   } catch (error) {}
+// };
+
+// btnDelete.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   deleteProduct();
+// });
